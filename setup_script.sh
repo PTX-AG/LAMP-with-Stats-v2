f5bfc51096d4d2f85a7be28920884b6654808da5
PTX-AG <armen@ptxm.net>
2025-07-23 22:34:46 -0600

    .

--- setup_script.sh
+++ setup_script.sh
@@ -152,8 +152,7 @@ install_nginx() {
             log "ERROR: make failed in brotli build."
             exit 1
         fi
-        cd ../../..
-        cd ../nginx-${NGINX_VERSION}
+        cd ../../../..
 
         # Configure NGINX
         ./configure \
